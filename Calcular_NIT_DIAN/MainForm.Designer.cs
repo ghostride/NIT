7cf706695812f632306746e847def3a328dbe90f
Ridemongo <gb_r>
2015-05-30 18:36:25 -0500

    update #1

--- Calcular_NIT_DIAN/MainForm.Designer.cs
+++ Calcular_NIT_DIAN/MainForm.Designer.cs
@@ -176,7 +176,7 @@ namespace Calcular_NIT_DIAN
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(654, 216);
+			this.ClientSize = new System.Drawing.Size(654, 207);
 			this.Controls.Add(this.label4);
 			this.Controls.Add(this.label3);
 			this.Controls.Add(this.label2);
